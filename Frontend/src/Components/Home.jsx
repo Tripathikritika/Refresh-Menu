fd405614b5150383fda4c623b5fa6bfabe8d8200
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-11-04 01:33:14 +0530

    Data Populated in Landing Page

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import LandingPage from './LandingPage'
 const { default: Navbar } = require("../Others/NavBar");
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <LandingPage />
     </>
   );
 };
--- Frontend/src/Components/LandingPage.jsx
+++ Frontend/src/Components/LandingPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useEffect } from 'react'
 import styles from './LandingPage.module.css'
-function LandingPage() {
+import axios from 'axios'
+import { useState } from 'react'
 
-    
+function LandingPage() {
+    const [ food_List , setFoodList ] = useState([])
+    useEffect ( () => {
+        axios.get(`http://localhost:8000/`) 
+             .then((res) => setFoodList(res.data) )
+             .catch((err) => console.log(err))
+    },[])
+    console.log(food_List)
     return (
         <>
         {/* First part */}
@@ -17,9 +26,10 @@ function LandingPage() {
                     <div>
                         <img src="https://s3-ap-southeast-1.amazonaws.com/foodvista.1/64f2ee38-bbba-460f-976b-cb60b532e507.jpg" height = "300px" alt="Bowls"/>
                     </div>
-                    {/* <div>
+                     <div>
                         <img src="https://s3-ap-southeast-1.amazonaws.com/foodvista.1/1635b32e-b727-489a-bcd6-870941b29c40.jpg" height = "300px" alt="Desert 40 % Off"/>
                     </div>
+                    {/*
                     <div>
                         <img src="https://s3-ap-southeast-1.amazonaws.com/foodvista.1/a8d84480-dc3b-42c2-80d3-d3c3f4f2740e.jpg" height = "300px" alt="Order Today"/>
                     </div> */}
@@ -29,69 +39,93 @@ function LandingPage() {
                     <div>
                         <div className = 'row'>
                             <div className="col-12">
-                                <div className="row" style={{position:'relative'  }} data-spy="scroll" data-target=".navbar" data-offset="50">
+                                <div className="row" style={{position:'relative' }} data-spy="scroll" data-target=".navbar" data-offset="50">
                                     <div id="list-example" className="list-group col-2"  >
-                                        <a className="list-group-item list-group-item-action" href="#list-item-1">Item 1</a>
-                                        <a className="list-group-item list-group-item-action" href="#list-item-2">Item 2</a>
-                                        <a className="list-group-item list-group-item-action" href="#list-item-3">Item 3</a>
-                                        <a className="list-group-item list-group-item-action" href="#list-item-4">Item 4</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-1">Appetizers</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-2">Match Day Combos</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-3">New & Exciting</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-4">Continental</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-5">Big Biriyani Co.</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-6">Fit N Fab</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-7">Tndian/ Thalis</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-8">Wok Station</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-9">Large Plates</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-10">Snacks</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-11">Wandercrust Pizzas</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-12">Dessert</a>
+                                        <a className="list-group-item list-group-item-action" href="#list-item-13">Beverage</a>
                                     </div>
 
-                                    <div data-offset="0" className="scrollspy-example col-10" style={{height:'400px',overflow:'scroll'}}>
-                                        <h4 id="list-item-1">Item 1</h4>
+                                    <div data-offset="0" className="scrollspy-example col-10" style={{overflow:'scroll'}}>
+                                        <h4 id="list-item-1">APPETIZERS</h4>
+                                        <div style ={{display:'grid',gridTemplateColumns : 'auto auto auto'}}>
+                                            {
+                                                food_List.filter((item) => item.category === 'Appetizers').map((item) =>  (
+                                                    <div >
+                                                        <img src={item.food_link} alt="Appetizers" style={{width:'80%' , margin :'5px'}}/>
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                        <h4 id="list-item-2">MATCH DAY COMBOS</h4>
+                                        <div style ={{display:'grid',gridTemplateColumns : 'auto auto auto'}}>
+                                            {
+                                                food_List.filter((item) => item.category === 'Match Day Combos').map((item) =>  (
+                                                    <div >
+                                                        <img src={item.food_link} alt="Match Day Combos" style={{width:'80%' , margin :'5px'}}/>
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                        <h4 id="list-item-3">NEW & EXCITING</h4>
+                                        <div style ={{display:'grid',gridTemplateColumns : 'auto auto auto'}}>
+                                            {
+                                                food_List.filter((item) => item.category === '"New & Exciting"').map((item) =>  (
+                                                    <div >
+                                                        <img src={item.food_link} alt="New & Exciting" style={{width:'80%' , margin :'5px'}}/>
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                        <h4 id="list-item-4">CONTINENTAL</h4>
                                         <p>
-                                        Quis anim sit do amet fugiat dolor velit sit ea ea do reprehenderit culpa duis. Nostrud aliqua ipsum fugiat minim proident occaecat excepteur aliquip culpa aute tempor reprehenderit. Deserunt tempor mollit elit ex pariatur dolore velit fugiat mollit culpa irure ullamco est ex ullamco excepteur.
-                                        Quis anim sit do amet fugiat dolor velit sit ea ea do reprehenderit culpa duis. Nostrud aliqua ipsum fugiat minim proident occaecat excepteur aliquip culpa aute tempor reprehenderit. Deserunt tempor mollit elit ex pariatur dolore velit fugiat mollit culpa irure ullamco est ex ullamco excepteur.
-                                        Quis anim sit do amet fugiat dolor velit sit ea ea do reprehenderit culpa duis. Nostrud aliqua ipsum fugiat minim proident occaecat excepteur aliquip culpa aute tempor reprehenderit. Deserunt tempor mollit elit ex pariatur dolore velit fugiat mollit culpa irure ullamco est ex ullamco excepteur.
-                                        Quis anim sit do amet fugiat dolor velit sit ea ea do reprehenderit culpa duis. Nostrud aliqua ipsum fugiat minim proident occaecat excepteur aliquip culpa aute tempor reprehenderit. Deserunt tempor mollit elit ex pariatur dolore velit fugiat mollit culpa irure ullamco est ex ullamco excepteur.
-                                        Quis anim sit do amet fugiat dolor velit sit ea ea do reprehenderit culpa duis. Nostrud aliqua ipsum fugiat minim proident occaecat excepteur aliquip culpa aute tempor reprehenderit. Deserunt tempor mollit elit ex pariatur dolore velit fugiat mollit culpa irure ullamco est ex ullamco excepteur.
-                                        Quis anim sit do amet fugiat dolor velit sit ea ea do reprehenderit culpa duis. Nostrud aliqua ipsum fugiat minim proident occaecat excepteur aliquip culpa aute tempor reprehenderit. Deserunt tempor mollit elit ex pariatur dolore velit fugiat mollit culpa irure ullamco est ex ullamco excepteur.
-                                        Quis anim sit do amet fugiat dolor velit sit ea ea do reprehenderit culpa duis. Nostrud aliqua ipsum fugiat minim proident occaecat excepteur aliquip culpa aute tempor reprehenderit. Deserunt tempor mollit elit ex pariatur dolore velit fugiat mollit culpa irure ullamco est ex ullamco excepteur.
-                                        Quis anim sit do amet fugiat dolor velit sit ea ea do reprehenderit culpa duis. Nostrud aliqua ipsum fugiat minim proident occaecat excepteur aliquip culpa aute tempor reprehenderit. Deserunt tempor mollit elit ex pariatur dolore velit fugiat mollit culpa irure ullamco est ex ullamco excepteur.
-
+                                           
                                         </p>
-                                        <h4 id="list-item-2">Item 2</h4>
+                                        <h4 id="list-item-5">BIG BIRIYANI CO.</h4>
                                         <p>
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-
+                                           
                                         </p>
-                                        <h4 id="list-item-3">Item 3</h4>
+                                        <h4 id="list-item-6">FIT N FAB.</h4>
                                         <p>
-                                            Sxfhrcfyjvhgfdszertyuiopl;l,kmjnhgvfdswertyuiokjhgfdsaqw3e4r5t6y7u8 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam officia non repellat eos, velit maxime quasi fuga dolor eius excepturi magni necessitatibus vero harum atque tenetur molestiae? Ipsam, voluptatum debitis!
+                                           
                                         </p>
-                                        <h4 id="list-item-4">Item 4</h4>
+                                        <h4 id="list-item-7">INDIAN / THALIS</h4>
                                         <p>
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
-                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore modi amet iure asperiores eligendi quae in recusandae aliquid repellat ratione laboriosam, tenetur obcaecati similique sapiente reprehenderit quam? Delectus, recusandae maiores!
-                                            Lorem ipsum dolor sit amet c
-                                            Lorem ipsum dolor sit amet c
+                                           
+                                        </p>
+                                        <h4 id="list-item-8">WOK STATION</h4>
+                                        <p>
+                                           
+                                        </p>
+                                        <h4 id="list-item-9">WOK STATION</h4>
+                                        <p>
+                                           
+                                        </p>
+                                        <h4 id="list-item-10">WOK STATION</h4>
+                                        <p>
+                                           
+                                        </p>
+                                        <h4 id="list-item-11">WOK STATION</h4>
+                                        <p>
+                                           
+                                        </p>
+                                        <h4 id="list-item-12">WOK STATION</h4>
+                                        <p>
+                                           
+                                        </p>
+                                        <h4 id="list-item-13">WOK STATION</h4>
+                                        <p>
+                                           
                                         </p>
                                     </div>
 
