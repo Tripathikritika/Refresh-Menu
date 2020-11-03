282b4d43ef4c58a27883a67e6917b8a2e2d6aadc
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-11-03 20:57:50 +0530

    Connecting Database

---
+++ Backend/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+const FoodLists = require('./models/Foods')
+const food_list = require('./foodList')
+const router = require('./routes/routes')
+
+const app = express()
+
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(
+    process.env.ATLAS_URI, { 
+        useNewUrlParser : true , 
+        useUnifiedTopology:true, 
+        useCreateIndex : true
+    },(err) => {
+    if( err ){
+        console.log("Connection to database Failed")
+    }
+    else{
+        console.log("Connection to database Successfully")
+        // FoodLists.insertMany(food_list)
+        //          .then(() => console.log("Inserted Successfully"))
+        //          .catch((err) => console.log(err))
+    }
+})
+
+app.use('/' , router)
+app.listen(8000 , () => {
+    console.log("Server is running successfully at port 8000")
+})
