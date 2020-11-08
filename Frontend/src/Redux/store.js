dbd346906a51dc3b0aff2998d1636dee83d90b1e
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-11-08 22:36:21 +0530

    Redux For Addon Modal

--- Frontend/src/Redux/store.js
+++ Frontend/src/Redux/store.js
@@ -1,19 +1,14 @@
-// thunks
-// redux 
-// react redux
-// axios
-// material ui
-// 
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducer from "./Login/reducer";
 import mapReducer from './MapIntegration/reducer'
 import foodReducer from './FoodList/reducer'
 import singleFoodReducer from './SingleFood/reducer'
+import cartItemReducer from './Cart/reducer'
 
 let composeEnhancers = compose;
 
-const rootreducer = combineReducers({ reducer, mapReducer, foodReducer,singleFoodReducer});
+const rootreducer = combineReducers({ reducer, mapReducer, foodReducer,singleFoodReducer,cartItemReducer});
 
 if (process.env.NODE_ENV !== "production") {
   composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
