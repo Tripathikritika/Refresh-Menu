3649f2eae58c103409d013ddd04a9afda8116475
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-11-11 01:09:47 +0530

    Search Page Toggle State Maintained

--- Frontend/src/Redux/FoodList/action.js
+++ Frontend/src/Redux/FoodList/action.js
@@ -32,3 +32,39 @@ export const getFoodList = ( payload ) => ( dispatch ) => {
              console.log(err)
          })
 }
+
+export const searchFoodListRequest = () => {
+    return{
+        type : actionConstant.SEARCH_FOOD_LIST_REQUEST
+    }
+}
+
+export const searchFoodListSuccess= ( payload ) => {
+    return{
+        type : actionConstant.SEARCH_FOOD_LIST_SUCCESS,
+        payload 
+    }
+}
+
+export const searchFoodListFailure = () => {
+    return{
+        type : actionConstant.SEARCH_FOOD_LIST_FAILURE
+    }
+}
+
+export const getsearchFoodList = ( payload ) => ( dispatch ) => {
+    dispatch ( searchFoodListRequest() )
+    axios.get(`http://localhost:8000/searchItem?filteredName=${payload}`)
+         .then(res => {
+             dispatch(searchFoodListSuccess(res.data))
+            //  console.log(res.data)
+         })
+         .catch(err => {
+             dispatch( searchFoodListFailure(err))
+             console.log(err)
+         })
+}
+
+export const toggleSearch = () => ({
+    type : actionConstant.SEARCH_FUNCTION
+})
