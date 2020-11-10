f854ea9db9000d8b1c3abfaf2617dd218a8fa965
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2020-11-11 01:10:31 +0530

    Small Changes

--- Frontend/src/Routes/Routing.jsx
+++ Frontend/src/Routes/Routing.jsx
@@ -4,6 +4,10 @@ import { Route, Switch } from "react-router-dom";
 import DiwaliGift from "../Components/DiwaliGift/DiwaliGift";
 import FoodDetails from '../Components/FoodDetails'
 import Checkout from '../Components/Checkout'
+import Location from '../Others/LocationNotFound'
+import Offers from '../Others/Offers'
+import Filter from '../Components/FilterComponent'
+
 
 const Routing = () => {
   return (
@@ -12,6 +16,10 @@ const Routing = () => {
       <Route path="/diwali-gift" component={DiwaliGift} />
       <Route path='/:name/product/:id' render={(props) => <FoodDetails {...props}/> }/>
       <Route path='/checkout' render={(props) => <Checkout {...props} /> } />
+      <Route path='/location' render={(props) => <Location {...props} /> } />
+      <Route path='/offers' render={(props) => <Offers {...props} /> } />
+      <Route path = '/filter' render={(props) => <Filter {...props} /> } />
+
     </Switch>
   );
 };
